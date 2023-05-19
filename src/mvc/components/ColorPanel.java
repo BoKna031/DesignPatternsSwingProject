fc15ab8e8590b829d9ead57753466b18c8aca405
Bozidar Arsic <40611211+BoKna031@users.noreply.github.com>
2023-05-19 17:56:18 +0200

    Bug fix - Change color of buttons in ColorPanel

--- src/mvc/components/ColorPanel.java
+++ src/mvc/components/ColorPanel.java
@@ -2,14 +2,13 @@ package mvc.components;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ColorPanel extends JPanel {
+public class ColorPanel extends JPanel implements ActionListener {
 
-
-    private final ButtonGroup colors = new ButtonGroup();
-
-    private Color innerColor = Color.RED;
-    private Color outerColor = Color.RED;
+    private Color innerColor;
+    private Color outerColor;
 
 
     private final JButton btnInnerColor = new JButton("InnerColor");
@@ -17,12 +16,16 @@ public class ColorPanel extends JPanel {
 
 
     public ColorPanel(){
+        innerColor = outerColor = Color.RED;
+
         btnInnerColor.setBackground(innerColor);
         btnOuterColor.setBackground(outerColor);
-        colors.add(btnInnerColor);
-        colors.add(btnOuterColor);
+
         this.add(btnInnerColor);
         this.add(btnOuterColor);
+
+        btnOuterColor.addActionListener(this);
+        btnInnerColor.addActionListener(this);
     }
 
     public Color getInnerColor() {
@@ -31,5 +34,16 @@ public class ColorPanel extends JPanel {
 
     public Color getOuterColor() {
         return outerColor;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == btnInnerColor) {
+            innerColor = JColorChooser.showDialog(this, "Choose your inner color", innerColor);
+            btnInnerColor.setBackground(innerColor);
+        } else if (e.getSource() == btnOuterColor) {
+            outerColor = JColorChooser.showDialog(this, "Choose your outer color", outerColor);
+            btnOuterColor.setBackground(outerColor);
+        }
     }
 }
